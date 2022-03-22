c1d2c2cd11433cafd642403bc3f62e04fd067a50
Brian <lerato1000@icloud.com>
2022-03-22 10:27:30 -0300

    ext

---
+++ 0x03-debugging/mygrades.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) 
+{ 
+	int my_grades[10];
+
+	my_grades[15]; 
+	my_grades[23]; 
+	my_grades[11]; 
+	my_grades[13]; 
+	my_grades[80]; 
+	my_grades[97]; 
+	my_grades[5]; 
+	my_grades[67]; 
+	my_grades[90]; 
+	my_grades[1]; 
+	printf("%d", my_grades[1]);
+	return 0;
+}
