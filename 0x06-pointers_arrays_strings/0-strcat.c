0f4606c389e3f2cff2bd8c8a05afb317fe4a2727
Brian <lerato1000@icloud.com>
2022-03-24 14:10:36 -0300

    trouble

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,10 @@ char *_strcat(char *dest, char *src)
 {
 char *temp = dest;
 for
-(;*temp != '\0' ;temp++);
+(*temp != '\0' temp++);
 
 for
-(;*src != '\0' ;src++);
+(*src != '\0' src++);
 
 {
 *temp = *src;
